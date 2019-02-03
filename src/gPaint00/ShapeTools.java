569031cadd3b40aaba3e1425247196ee6ee24fb2
AmirHossein Askari <Kollohouser@gmail.com>
2019-02-03 13:05:32 +0330

    init all files in one step:|

---
+++ src/gPaint00/ShapeTools.java
@@ -0,0 +1,8 @@
+package gPaint00;
+
+public class ShapeTools {
+    public static final int Line = 1;
+    public static final int Rect = 2;
+    public static final int Oval = 3;
+    public static final int Pencil = 4;
+}
